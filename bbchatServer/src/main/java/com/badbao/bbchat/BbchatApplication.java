636ebdf331a4e8660f5dbf87c594c8a699e23173
Yicheng. Bao <60834356+thisisbadBao@users.noreply.github.com>
2021-05-13 19:40:55 +0800

    upload files

---
+++ bbchatServer/src/main/java/com/badbao/bbchat/BbchatApplication.java
@@ -0,0 +1,13 @@
+package com.badbao.bbchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbchatApplication.class, args);
+    }
+
+}
---
+++ bbchatServer/src/test/java/com/badbao/bbchat/BbchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badbao.bbchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
