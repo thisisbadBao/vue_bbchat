eb400389233615e54a4d37e51f2a116663dd15b3
Yicheng. Bao <60834356+thisisbadBao@users.noreply.github.com>
2021-05-19 23:38:48 +0800

    界面优化

--- bbchat/src/App.vue
+++ bbchat/src/App.vue
@@ -11,11 +11,13 @@ export default {
 </script>
 
 <style>
+bodd{
+  height: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 </style>
--- bbchat/src/components/home/AppIndex.vue
+++ bbchat/src/components/home/AppIndex.vue
@@ -1,67 +1,40 @@
 <template>
-  <div>
+  <div class="main">
     <div class="wrapper">
-      <div class="box header">bbChat, just bb.</div>
-      <div class="box sidebar">
-        <el-aside class="sideBar" width="230px">
-          <div class="title">聊天室列表</div>
-          <el-row class="tac" >
-            <el-col width="230">
-              <el-menu
-                default-active="2"
-                class="el-menu-vertical-demo"
-                @open="handleOpen"
-                @close="handleClose"
-                background-color="#545c64"
-                text-color="#fff"
-                active-text-color="#ffd04b">
-                <el-submenu index="1">
-                  <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span>导航一</span>
-                  </template>
-                  <el-menu-item-group>
-                    <template slot="title">分组一</template>
-                    <el-menu-item index="1-1">选项1</el-menu-item>
-                    <el-menu-item index="1-2">选项2</el-menu-item>
-                  </el-menu-item-group>
-                  <el-menu-item-group title="分组2">
-                    <el-menu-item index="1-3">选项3</el-menu-item>
-                  </el-menu-item-group>
-                  <el-submenu index="1-4">
-                    <template slot="title">选项4</template>
-                    <el-menu-item index="1-4-1">选项1</el-menu-item>
-                  </el-submenu>
-                </el-submenu>
-                <el-menu-item index="2">
-                  <i class="el-icon-menu"></i>
-                  <span slot="title">导航二</span>
-                </el-menu-item>
-                <el-menu-item index="3" disabled>
-                  <i class="el-icon-document"></i>
-                  <span slot="title">导航三</span>
-                </el-menu-item>
-                <el-menu-item index="4">
-                  <i class="el-icon-setting"></i>
-                  <span slot="title">导航四</span>
-                </el-menu-item>
-                <el-menu-item index="5">
-                  <i class="el-icon-setting"></i>
-                  <span slot="title">导航四</span>
-                </el-menu-item>
-                <el-menu-item index="6">
-                  <i class="el-icon-setting"></i>
-                  <span slot="title">导航四</span>
-                </el-menu-item>
-              </el-menu>
-            </el-col>
-          </el-row>
-        </el-aside>
-      </div>
+      <div class="box header">Hello world, this is bbChat.</div>
+      <el-aside class="box sidebar">
+        <el-menu :default-openeds="[]">
+          <el-menu-item-group>
+            <template slot="title">房间列表</template>
+            <el-menu-item index="1-1">room1</el-menu-item>
+            <el-menu-item index="1-2">room2</el-menu-item>
+            <el-menu-item index="1-2">room3</el-menu-item>
+            <el-menu-item index="1-2">room4</el-menu-item>
+          </el-menu-item-group>
+        </el-menu>
+      </el-aside>
       <div class="box content">
         <div class="chatWrapper">
-          <div class="messageBox">消息</div>
-          <el-input v-model="messageInput" aria-placeholder="请输入要发送的内容" class="messageInputBox"></el-input>
+          <el-tabs class="messageBox" stretch="true">
+            <el-tab-pane >
+              <span slot="label"><i class="el-icon-chat-line-round"></i></span>
+              <div>消息区</div>
+            </el-tab-pane>
+            <el-tab-pane>
+              <span slot="label"><i class="el-icon-user-solid"></i></span>
+              用户列表
+            </el-tab-pane>
+          </el-tabs>
+          <div class="messageInputBox">
+            <el-form :inline="true" :model="formInline" class="demo-form-inline">
+              <el-form-item>
+                <el-input v-model="messageInput" aria-placeholder="输入你想发送的内容" style="width: 600px"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="sendMessage">发送</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
         </div>
       </div>
       <div class="box footer">byc wbj xr yzl@SE2021</div>
@@ -78,6 +51,9 @@ export default {
     }
   },
   methods: {
+    sendMessage () {
+      console.log('sent!')
+    },
     handleOpen (key, keyPath) {
       console.log(key, keyPath)
     },
@@ -90,49 +66,60 @@ export default {
 
 <style>
 
-/*聊天界面*/
+
+.main{
+  height: 100vh;
+}
+.wrapper{
+  height: 100%;
+  width: 100%;
+  background: url("background.png") no-repeat center;
+  background-size: cover;
+  position: fixed;
+}
 
 /*消息界面*/
 .messageBox {
   grid-area: messageBox;
-  background-color: #42b983;
-  width: 600px;
-  height: 400px;
-  position: absolute;
-  left: 100px;
-  right: 10px;
-  top: -60px;
+  margin: 10px 10px 5px 10px;
+  border-radius: 10px;
+  /*border: 3px solid #444444;*/
+  padding: 5px;
+  background: url("message_bg.png");
 }
 
 .messageInputBox{
   grid-area: messageInputBox;
+  margin: 1px 10px 10px 10px;
+  /*border: 3px solid #444444;*/
 }
 
 .chatWrapper{
   display: grid;
   grid-gap: 5px;
-  grid-template-columns: 300px;
-  grid-template-rows: 100px 300px 50px;
+  /*grid-template-columns: 1fr 9fr 1fr;*/
+  grid-template-rows: 5fr 1fr;
+  height: 100%;
   grid-template-areas:
-    "......."
     "messageBox"
     "messageInputBox";
-  background-color: #505458;
-  color: #444;
-  position: relative;
+  /*border: #26BCD5 solid;*/
+  border-radius: 10px;
+  margin-right: 5px;
+  color: #444444;
 }
 
-/*以下为布局*/
-
-.sideBar{
-  margin-left: 0px;
-}
+/*以下为总体布局*/
 body {
-  margin: 10px;
+  margin: 0px;
 }
 
 .sidebar {
   grid-area: sidebar;
+  /*border: #444444 solid;*/
+  overflow: auto;
+  padding: 8px;
+  background: url("list_bg.png");
 }
 
 .content {
@@ -149,27 +136,26 @@ body {
 
 .wrapper {
   display: grid;
-  grid-gap: 10px;
-  grid-template-columns: 235px 520px 520px;
-  grid-template-rows: 30px 600px 30px;
+  grid-gap: 5px;
+  grid-template-columns: 2fr 4fr 4fr;
+  grid-template-rows: 1fr 18fr 1fr;
   grid-template-areas:
-    "....... header header"
+    "header header header"
     "sidebar content content"
     "footer  footer  footer";
   background-color: #fff;
   color: #444;
+  height: 100vh;
 }
 
 .box {
-  background-color: #444;
-  color: #fff;
+  color: #1C7887;    /*字体颜色*/
   border-radius: 5px;
-  padding: 0px;
   font-size: 150%;
 }
 
 .header,
 .footer {
-  background-color: #999;
+  background-color: #A7DDE0;
 }
 </style>
