19a93ea3a60395d0c40cb39f74279f81311110a3
Yicheng. Bao <60834356+thisisbadBao@users.noreply.github.com>
2021-05-24 21:45:12 +0800

    test

--- bbchatServer/bbchat/src/main/java/com/bbchat/dao/entity/Account.java
+++ bbchatServer/bbchat/src/main/java/com/bbchat/dao/entity/Account.java
@@ -6,9 +6,12 @@ import javax.persistence.Id;
 
 @Data
 public class Account {
+<<<<<<< HEAD
 
     private String accountId;
 
+=======
+>>>>>>> parent of 8b2d519 (add webSocket)
     @Id
     private String accountid;
 
@@ -20,4 +23,8 @@ public class Account {
 
     private Boolean able;
 
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> parent of 8b2d519 (add webSocket)
