d7b5e2d45c9957d17169b2c652e19f53aef2df91
Yicheng. Bao <60834356+thisisbadBao@users.noreply.github.com>
2021-05-27 00:59:56 +0800

    add avatar

--- bbchatServer/bbchat/src/main/java/com/bbchat/common/CONST.java
+++ bbchatServer/bbchat/src/main/java/com/bbchat/common/CONST.java
@@ -2,5 +2,5 @@ package com.bbchat.common;
 
 public class CONST {
     public static final String avatar =
-            "https://seu-talent-cloud-1304015296.cos.ap-nanjing.myqcloud.com/heaImage.png";
+            "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png\"";
 }
